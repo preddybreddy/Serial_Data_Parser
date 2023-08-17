7bb1b776709d2b3634134e60d4285ee9ce258dcf
Pranay Byreddy <Pranay.Byreddy@trane.com>
2023-08-17 10:16:00 -0400

    refactored exe code

--- selay_scab_hs.py
+++ selay_scab_hs.py
@@ -1,31 +1,9 @@
-import serial
-import time
 import Utils_.utils as utils
 import Utils_.utils_selay_ls as utils_selay_ls
 import Utils_.utils_selay_hs as utils_selay_hs
-import os
 from tabulate import tabulate
 import tkinter as tk
 
-
-def get_valid_input(serial_obj):
-    lines_from_ULC = []
-    first_line = serial_obj.readline().decode('ASCII')
-    while 'Total Run Time' not in first_line:
-        first_line = serial_obj.readline().decode('ASCII')
-
-    lines_from_ULC.append(first_line)    
-    first_line = serial_obj.readline().decode('ASCII')
-    while ('*' not in first_line):
-        lines_from_ULC.append(first_line)
-        first_line = serial_obj.readline().decode('ASCII')
-    return lines_from_ULC
-
-def strip_line(input_list):
-    return list(map(lambda x: x.strip(), input_list))
-
-def remove_empty_string(input_list):
-    return list(filter( lambda x: x != '', (filter(lambda x: x.isspace() == False, input_list))))
 
 def generate_table(current_data_from_ULC_formatted):
     head = ['Parameter', '\"Raw Data\"/DISPLAY']
@@ -59,32 +37,6 @@ def generate_table(current_data_from_ULC_formatted):
 #        first_line = lines[i]
 #    return lines_from_ULC
 
-def configure_com_port(com_port):
-    serial_obj = serial.Serial(f'com{com_port}')
-    serial_obj.baudrate = 9600
-    serial_obj.bytesize = 8
-    serial_obj.parity = 'N'
-    serial_obj.stopbits = 1
-    return serial_obj
-
-def output_table(com_port):
-    serial_obj = configure_com_port(com_port)
-    prev_time = 0
-    while(True):
-        output_set = get_valid_input(serial_obj)
-        output_set_stripped = strip_line(output_set)
-        output_set_empty_strings_removed = remove_empty_string(output_set_stripped)
-        curr_time = utils.extract_time(output_set_stripped[0])
-        #print(curr_time)
-        if not(prev_time == 0 or abs(curr_time - prev_time) == 10):
-            print('Data is not synchronized with 10-sec intervals\n')
-            print('Restart process')
-        # output table
-        os.system('cls')
-        generate_table(output_set_empty_strings_removed)
-        prev_time = curr_time
-        time.sleep(9.5)
-
 
 if __name__ == '__main__':
     window = tk.Tk()
@@ -105,7 +57,7 @@ if __name__ == '__main__':
     button.pack()
     def get_com_port(event):
         com_port = com_entry.get()
-        output_table(com_port)
+        utils.output_table(com_port, generate_table)
     button.bind('<Button-1>', get_com_port)
     
     window.mainloop()
